ab089c6bc6d7cf4c58b4ffb6df7397b070483aec
Nguyễn Quốc Hùng <qihu079@gmail.com>
2025-10-10 01:01:02 +0700

    Update README, product API and MainLayout

--- web/src/api/product.js
+++ web/src/api/product.js
@@ -1,15 +1,1 @@
-// const BASE_URL = "https://fakestoreapi.com";
-
-// export const productApi = {
-//   async getAll() {
-//     const res = await fetch(`${BASE_URL}/products`);
-//     if (!res.ok) throw new Error("Failed to fetch products");
-//     return res.json();
-//   },
-
-//   async getById(id) {
-//     const res = await fetch(`${BASE_URL}/products/${id}`);
-//     if (!res.ok) throw new Error("Failed to fetch product details");
-//     return res.json();
-//   },
-// };
+//test ở đây thôi ko có call api gì hết
